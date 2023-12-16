a042344e33305dd2feee811ad0f13d79476016d5
Marti-yan <yanloufraga2@gmail.com>
2023-12-15 23:07:41 -0300

    finalização da page da mesa

--- src/pages/MesaRadionica/index.jsx
+++ src/pages/MesaRadionica/index.jsx
@@ -1,14 +1,207 @@
-import Nav from "../../components/Nav";
-import Header from "../../components/Header";
-
-
 const MesaRadionica = () => {
   return (
     <>
-      <h1>Mesa Radionica</h1>
-      <section></section>
+      <h1>Mesa Radiônica e Radiestesia</h1>
+      <section>
+        <span>
+          <img src="./pages/mesaRadionica/01.jpg" alt="" />
+          <h5>MESA RADIÔNICA DOS RELACIONAMENTOS:</h5>
+          <p>
+            Essa mesa é indicada para solteiros, que não conseguem encontrar um
+            amor.
+          </p>
+          <p>
+            Para casais que necessitam de proteção e/ou fortalecer seu
+            relacionamento, ou todos aqueles que desejam receber a energia mais
+            poderosa do Universo, que é o puro amor da Chama Rosa da Mestra
+            Rovena!
+          </p>
+          <p>
+            Ajudar o consulente encontrar sua alma gêmea;Fortalecer
+            relacionamentos conjugais;Resolver problemas no casamento, namoro,
+            família, amizades;
+          </p>
+          <p>
+            Promover a libertação de relações abusivas;Ajuda a atrair novas e
+            boas amizades;
+          </p>
+          <p>
+            Remove obstáculos que impedem o bom relacionamento do casal;Protege
+            o relacionamento de influências negativas;
+          </p>
+          <p>Ajuda o consulente a cumprir seu propósito e vida;</p>
+          <p>
+            Trabalha questões de vidas passadas, pendências de antigos
+            relacionamentos;
+          </p>
+          <p>
+            Aumenta o amor por si mesmo, o amor próprio, trabalha a auto
+            aceitação;
+          </p>
+          <p>
+            Ajuda a remover da mente o medo de se relacionar, de tentar outra
+            vez depois de uma decepção;
+          </p>
+          <p>
+            Esses são só alguns benefícios, pois a Mesa pode ajudar ainda em
+            várias outras questões da vida do consulente, mediante a capacidade
+            energética
+          </p>
+        </span>
+        <br />
+        <span>
+          <img src="./pages/mesaRadionica/02.jpg" width="80%" alt="" />
+          <h5>MESA RADIÔNICA DA PROSPERIDADE:</h5>
+          <p>Realiza Limpeza de crenças limitantes relacionadas aodinheiro;</p>
+          <p>
+            Ajuda o consulente no equilíbrio financeiro, fazendo assim que o
+            mesmo evite gastos desnecessários;
+          </p>
+          <p>
+            Afasta energias negativas, olho gordo, inveja, magias queestejam
+            travando a vida financeira;- Aumenta a confiança do consulente em
+            sua própriacapacidade, despertando o lado empreendedor e/ou
+            forçainterior do mesmo;
+          </p>
+          <p>
+            Ajuda o consulente no equilíbrio financeiro, fazendo assimque o
+            mesmo evite gastos desnecessários;Ajuda o consulente a ter uma
+            probabilidade maior depassar em concursos, provas, e processos
+            seletivos;- Abre os caminhos para prosperidade;
+          </p>
+          <p>
+            Busca solucionar problemas de vidas passadas quepossam estar
+            impedindo a prosperidade do consulentenesta vida;
+          </p>
+          <p>
+            Materializa o financeiro, traz Sorte e Fartura;- Muda a consciência
+            do consulente em relação aodinheiro, o tornando um ser humano mais
+            próspero;
+          </p>
+          <p>
+            Identifica possíveis problemas que podem estar impedindo a boa
+            fluidez do dinheiro;
+          </p>
+          <p>
+            Atrai prosperidade em todas as áreas da vida, tornando oconsulente
+            próspero no dinheiro, mais também no amor, nasaúde física entre
+            outros;
+          </p>
+        </span>
+        <br />
+        <span>
+          <img src="./pages/mesaRadionica/03.jpg" alt="" />
+          <h5>
+            A Mesa Radiônica Akáshica pode ser usada para tratar diversos
+            assuntos tais como:
+          </h5>
+          <p>
+            -Trabalha o atendido na frequência de suas 12 dimensões
+            reequilibrando fragmentos de alma em dimensões inferiores e ativando
+            sua conexão com níveis superiores de alta frequência atuando
+            diretamente na mudança de consciência e cura da alma em todos os
+            seus registros fragmentados, realizando a Cura Akáshica total.
+          </p>
+          <p>
+            -Problemas de saúde e baixa energia (aumento da frequência/imunidade
+            física, mental, emocional e espiritual, alinhamento dos chacras,
+            reequilíbrio da aura, em qualquer linha do tempo com a qual o
+            atendido possa estar em conexão.
+          </p>
+          <p>
+            -Limpeza de registros akáshicos negativos e de toda energia criada
+            de todas as vivências do ser influenciando negativamente a vida
+            atual aqui/agora. A limpeza desses registros melhora imediatamente
+            não só a vida atual do atendido, mas também de todos os seus níveis
+            ligados a qualquer espaço dimensional, vidas passadas, paralelas,
+            simultâneas, interdimensionais.
+          </p>
+          <p>
+            -Bloqueios (conscientes ou inconscientes – do presente, passado,
+            paralelo e futuro).
+          </p>
+          <p>
+            -Remoção de condicionamento negativo, crenças limitantes adquiridas
+            e herdadas, vícios e padrões antigos, como medo, culpa e vergonha
+            ligados aos nossos níveis em dimensões nferiores.
+          </p>
+          <p>
+            -Questões emocionais (problemas de relacionamento, família,
+            parcerias, etc.).
+          </p>
+          <p>
+            -Autodesenvolvimento, habilidades psíquicas e conexões com dons
+            adquiridos em outras imensões.
+          </p>
+          <p>
+            – Conexão com o propósito de vida, onde haverá um alinhamento ao
+            propósito existencial do consulente, para que ele o desenvolva da
+            melhor forma possível e para o bem maior de todos os envolvidos.
+          </p>
+          <p>
+            -Aumentar a autoestima e a confiança trabalhando o desapego
+            oportunizando a entrega ao amor verdadeiro.
+          </p>
+          <p>
+            -Tratar animais, plantas e solo que necessitam de cura akáshica e
+            equilíbrio energético.
+          </p>
+        </span>
+      </section>
+      <br />
+      <section>
+        <span>
+          <img src="./pages/mesaRadionica/04.jpg" alt="" width="40%" />
+          <img src="./pages/mesaRadionica/05.jpg" alt="" />
+        </span>
+        
+          <span>
+            <p>
+              A Radiestesia é a arte, ou ciência, que investiga as radiações
+              naturais dos objetos e seres vivos. A mesa Radiônica é a ciencia,
+              ou arte, capaz de trabalhar tais energias, de forma a
+              equilibra-las e harmonizá-las.
+            </p>
+            <p>
+              A mesa radiônica é uma tábua composta por vários gráficos e
+              símbolos especialmente desenvolvida para trabalhar a limpeza,
+              equilíbrio e harmonização de pessoas, situações, objetos e
+              ambientes. Com a mesa e o pêndulo é possível trabalhar com a
+              radiestesia, fazendo medições e investigações e com a radiônica,
+              fazendo as limpezas e harmonizações que forem necessárias. A
+              Radiestesia é utilizada para descobrir os desequilíbrios
+              energéticos, já a mesa radiônica equilibra tudo aquilo que tem
+              necessidade de ser equilibrado.
+            </p>
+          </span>
+      </section>
+      <section>
+        <span>
+        <img src="./pages/mesaRadionica/06.jpg" alt="" />
+        <h5>Com a consulta da mesa radiônica somos capazes de:</h5>
+        <p>- equilibrar e harmonizar os corpos físico, mental, emocional e energético;</p>
+        <p>- fazer a limpeza e harmonização de pessoas, objetos e ambientes;</p>
+        <p>- fazer a limpeza e harmonização dos chakras;</p>
+        <p>- harmonizar todos os tipos de relacionamentos;</p>
+        <p>- acelerar um processo judicial;</p>
+        <p>- potencializar uma situação (encontrar trabalho, comprar, vender,alugar etc);</p>
+        <p></p>
+        <p>- ativar a percepção extra-sensorial;</p>
+        <p>- transmutar padrões mentais e comportamentais (medos, traumas etc);</p>
+        <p>- limpar as energias residenciais e/ou local de trabalho;</p>
+        <p>- verificar e ‘liberar’ (caso permitido) os votos, pactos, contratos, promessas, compromissos desta vida e vidas passadas;</p>
+        <p>- limpar e harmonizar as causas e origens das doenças, bem como potencializar o processo de cura e recuperação</p>
+        <p>- acelerar o efeito de tratamento com medicamentos e neutralizar os efeitos colaterais dos mesmos;</p>
+        <p>- transmuta padrões negativos em relação a finanças, trabalho, relacionamentos etc.</p>
+        </span>
+        <br />
+        <br />
+        <span>
+        Todo trabalho é feito sempre dentro do que é Divino para que as soluções e resultados seja o MELHOR para TODOS os envolvidos dentro de uma situação. É uma terapia de limpeza energética. Em momento algum ela substitui o tratamento médico convencional ou a consulta com um profissional médico credenciado.
+        </span>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default MesaRadionica
+export default MesaRadionica;
--- src/pages/TarotdosAnjos/index.jsx
+++ src/pages/TarotdosAnjos/index.jsx
@@ -1,14 +1,215 @@
-import Nav from "../../components/Nav";
-import Header from "../../components/Header";
-
+import style from "../TarotdosAnjos/tarotanjos.module.css";
 
 const TarotAnjos = () => {
   return (
     <>
+      <h1> tarot dos anjos </h1>
+      <section>
+        <div>
+          <span>
+            <img src="./pages/tarotdosanjos/01.jpg" alt="" />
+            <p>Ritual para a ancoragem dos anjos cabalísticos</p>
+            <p>
+              O seguinte ritual, ensinado por Monica Buonfiglio no livro “A
+              magia dos anjos cabalísticos”, é um meio poderoso para estabelecer
+              uma ligação intensa com os anjos e pedir a ajuda deles em qualquer
+              situação. Pode ser utilizado para a purificação astral de algum
+              lugar, para o equilíbrio energético ou para fazer algum pedido.
+              Deve ser realizado durante sete dias seguidos.
+            </p>
+          </span>
+          <span>
+            <p>
+              Em primeiro lugar, você deve conhecer o nome de seu anjo
+              cabalístico e do príncipe que governa a categoria da qual ele faz
+              parte.
+            </p>
+            <p>
+              Você também vai precisar de um prato branco de porcelana, sete
+              velas comuns, cada vela tendo uma das seguintes cores: branco,
+              azul, amarelo, verde, vermelho, rosa e lilás, e sete folhas de
+              papel com o seguinte desenho:
+            </p>
+            <img src="./pages/tarotdosanjos/02.png" alt="" />
+            <p>
+              Antes de começar o ritual, prepare o ambiente acendendo um
+              incenso. Em seguida, coloque o papel com o desenho sobre o prato.
+              No primeiro dia, a vela utilizada será a branca. Você deve ungi-la
+              com óleo essencial ou óleo comum. Depois, fixe-a sobre o desenho,
+              bem no centro, onde os dois cículos encontram-se. Acenda a vela,
+              invoque seu Príncipe e seu anjo e recite o Salmo correspondente.
+            </p>
+            <img src="./pages/tarotdosanjos/03.png" alt="" />
+            <p>
+              Depois, deixe a vela queimar até o fim. Se, no final, o fogo da
+              vela queimar o papel, mesmo que uma pequena parte dele, é sinal de
+              que você conseguiu conectar-se a seu anjo e pode continuar a
+              realização do ritual com as outras velas.
+            </p>
+            <p>
+              Caso o papel não queime, você deve usar outra vela branca no dia
+              seguinte. Apenas quando a vela branca queimar o papel, você deve
+              passar para a cor seguinte.
+            </p>
+            <p>
+              Se o papel queimar mais para o lado do círculo direito, chamado OD
+              (círculo da inteligência), a solução será obtida de forma
+              inteligente. Já se o papel queimar mais do lado esquerdo, chamado
+              OB (círculo do instinto), seu anjo estará lutando contra seu gênio
+              contrário, afastando-o astralmente de sua casa, até então,
+              carregada. Se o papel queimar, pelo menos num pequeno pedaço, é
+              sinal de que o anjo conseguiu ancorar e o pedido feito por você
+              será atendido.
+            </p>
+            <p>
+              No segundo dia, escolha outra cor de vela e repita o mesmo
+              procedimento, e assim por diante, até o sétimo dia. Apenas com a
+              vela branca você deverá observar a queima ou não do papel, com as
+              outras cores não é necessário. O que sobrar das folhas de papel,
+              você deverá queimar e soprar as cinzas ao vento. Se sobrar cera
+              depois da vela queimada, enterre-a ou jogue-a em água corrente.
+              Faça a mesma coisa se o prato quebrar.
+            </p>
+            <p>
+              Se você interromper por algum motivo a realização do ritual antes
+              de finalizá-lo, você deverá recomeçar a partir da vela branca.
+            </p>
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src="./pages/tarotdosanjos/04.jpg" alt="" />
+            <h4>
+              AO ACENDER UMA VELA,É POSSÍVEL IDENTIFICAR ALGUMAS MENSAGENS:
+            </h4>
 
-    <div> tarot dos anjos </div>
-  </>
-  )
-}
+            <p>
+              <b>Vela que não acende prontamente: </b>O anjo pode estar tendo
+              dificuldades para ancorar .O astral ao seu redor pode estar
+              “poluído”.
+            </p>
+            <p>
+              <b>Vela queimando com luz azulada: </b>
+              Indica a presença de anjos e fadas. É um bom sinal.
+            </p>
+            <p>
+              <b>Chama vacilante: </b>O anjo demonstra que, devido às
+              circunstâncias, seu pedido terá algumas mudanças.
+            </p>
+            <p>
+              <b>Chama que levanta e abaixa:</b>
+              Você está pensando em várias coisas ao mesmo tempo. Sua mente pode
+              estar um pouco tumultuada.
+            </p>
+            <p>
+              <b>Chama que solta fagulhas no ar: </b>O anjo colocará alguém no
+              seu caminho para comunicar o que deseja. Poderá ter algum tipo de
+              desapontamento antes do pedido ser realizado.
+            </p>
+            <p>
+              <b>Chama que parece uma espiral: </b>
+              Seus pedidos serão alcançados. O anjo está levando sua mensagem.
+            </p>
+            <p>
+              <b>Pavio que se divide em dois: </b>O pedido foi feito de forma
+              dúbia.
+            </p>
 
-export default TarotAnjos
+            <p>
+              <b>Ponta do pavio brilhante:</b>
+              Sorte e sucesso no pedido.
+            </p>
+            <p>
+              <b> Vela que chora muito: </b>O anjo sente dificuldades em
+              realizar seu pedido.
+            </p>
+            <p>
+              <b>Sobra um pouco de pavio e a cera fica em volta: </b>O anjo pede
+              mais oração.
+            </p>
+            <p>
+              <b> Se a vela apaga: </b>O anjo ajudará na parte mais difícil do
+              pedido, o resto cabe a você resolver.
+            </p>
+          </span>
+          <span>
+            <p>
+              Quando visualizamos uma determinada cor, nosso cérebro atua para
+              que a aura a reflita. É através desse processo que o anjo irá
+              captar e entender nossos pedidos. Para cada tipo de pedido é
+              utilizado uma cor de vela. Este é o primeiro elo de ligação com os
+              anjos. (Monica buonfiglio).
+            </p>
+            <p>
+              {" "}
+              <b>OBS;</b> NESTE BLOG VC ENCONTRARÁ O NOME DE SEU PRÍNCIPE,SEU
+              ANJO,E O SALMO CORRESPONDENTE AO SEU DIA DE NASCIMENTO.
+            </p>
+            <a
+              className={style.link}
+              href="https://tarodosanjosedicasdoanjododia.blogspot.com/"
+            >
+              tarodosanjos-Blog
+            </a>
+          </span>
+        </div>
+        <br />
+        <div>
+          <span>
+            <p>
+              Colocar o nome do príncipe no papel Abaixo o nome de seu anjo de
+              guarda logo abaixo o seu nome de batismo.
+            </p>
+            <p>
+              A vela precisa ser untada por que o azeite é sagrado, pelas
+              escrituras, pois ele destila nas lamparinas que ardem à face de
+              deus.
+            </p>
+            <p>
+              Olear a vela é fazer um elo psíquico entre você ela através do
+              tato você está passando energia e, dessa forma, a vela funciona
+              como uma extensão da mente.
+            </p>
+            <p>Acenda um incenso com um isqueiro.</p>
+            <p>
+              faça o salmo do seu dia de nascimento e escreva na parte da baixo
+              do desenho da lemniscata seu pedido e no final a frase:{" "}
+              <i>"Bendito é o meu pedido pois ele é atendido."</i>{" "}
+            </p>
+          </span>
+        </div>
+        <div>
+          <span>
+            <img src="./pages/tarotdosanjos/05.jpg" alt="" />
+            <h5>NO 1º DIA DA VELA BRANCA:</h5>
+            <p>
+              Representa pureza e sinceridade, é utilizada para paz de espírito,
+              harmonia e equilibrio em nossas casas. (lembre-se se não queimar o
+              papel nem que seja um pouco, terá de refazer)
+            </p>
+            <p>
+              Verdade, tranquilidade e compreensão, relacionado ao trabalho e
+              prosperidade, negócios.
+            </p>
+            <h5>VELA AMARELA:</h5>
+            <p>
+              Vida, alegria, força, entusiasmo, vontade de viver. simboliza
+              estudos, deve ser usada para pedir sobre estas situações;
+            </p>
+            <h5>VELA ROSA:</h5>
+            <p>
+              Beleza, amor, deve ser usada para pedir sobre assuntos amorosos,
+              para fortalecer laços afetivos e filiais.
+            </p>
+            <h5>VELA VERDE:</h5>
+            <p>Calma, tranquilidade, saúde, equilíbrio.</p>
+            <img src="./pages/tarotdosanjos/06.jpg" alt="" />
+            <h5>VELA VIOLETA OU LILÁS:</h5>
+          </span>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TarotAnjos;
